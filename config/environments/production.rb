ff52f7568fab483737b4273e9b338bda01a5e062
Jarys J. Satre <jarys.satre@gmail.com>
2018-01-16 16:16:54 -0700

    implemented actioncable to work with heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https:/jsportfolio.herokuapp.com', 
+    'http://jsportfolio.herokuapp.com']
+  config.action_cable.url = "wss://jsportfolio.herokuapp.com/cable"
 end
