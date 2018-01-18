373716a3bb69108465fb80298e13c5c13c0266ac
Jarys J. Satre <jarys.satre@gmail.com>
2018-01-17 17:18:10 -0700

    action cable fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,9 +14,11 @@
 //= require jquery_ujs
 //= require jquery-ui
 //= require popper
+//= require rails-ujs
 //= require html.sortable
 //= require cocoon
 //= require gritter
 //= require cable
+//= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
